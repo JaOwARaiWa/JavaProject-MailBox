9d52fc0f7b9bf744923c16dbc9bb67717b6c0a4c
JaOwARaiWa <kiattikun.tho@ku.th>
2020-09-21 16:33:17 +0700

    add maven to project

---
+++ src/main/java/condo/CondoController.java
@@ -0,0 +1,6 @@
+package condo;
+
+public class CondoController
+{
+    //...coding in progress...
+}
---
+++ src/main/java/condo/MainProgramCondo.java
@@ -0,0 +1,24 @@
+package condo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainProgramCondo extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/condofirstpage.fxml"));
+        primaryStage.setTitle("6210450032");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
